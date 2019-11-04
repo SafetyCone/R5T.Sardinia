aabc50a8cbe6293fb3281587cfda86e9901c3310
MinexAutomation <minex.automation@gmail.com>
2019-11-03 19:24:12 -0800

    Added DI-container services collection Configure() extension method that just uses the whole configuration as the configuration to bind against.

--- source/R5T.Sardinia/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Sardinia
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Sardinia/Code/Extensions/DI/IServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using R5T.Dacia.Extensions;
+
+
+namespace R5T.Sardinia
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IConfiguration GetConfiguration(this IServiceCollection services)
+        {
+            var configuration = services.GetIntermediateRequiredService<IConfiguration>();
+            return configuration;
+        }
+
+        public static IServiceCollection Configure<TOptions>(this IServiceCollection services)
+            where TOptions: class // Must be a reference type for the generic parameter of the Configure() method.
+        {
+            var configuration = services.GetConfiguration();
+
+            services
+                .Configure<TOptions>(configuration)
+                ;
+
+            return services;
+        }
+    }
+}
